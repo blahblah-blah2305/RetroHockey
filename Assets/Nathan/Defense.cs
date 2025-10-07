c9a0f743fb3f947af158a43e5439b528eb57c6d9
nasawyer7 <nasawyer7@gmail.com>
2025-10-07 09:29:20 -0700

    added some sudo code and bad d players

---
+++ Assets/Nathan/Defense.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+public class Information
+{
+    //takes in info
+}
+
+public class OffensivePlayer
+{
+    //protected Play currentPlay;
+
+    public void DecideAction(Information info)
+    {
+        //decides what to do
+    }
+
+    public void Execute(Action action)
+    {
+        //executes chosen play
+    }
+}
+
